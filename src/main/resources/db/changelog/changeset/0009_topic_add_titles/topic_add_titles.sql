519bb4dc785618d764865dad11b08dcc990c1e8e
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-07-05 12:39:56 +0200

    chore: make topic titles nullable field

--- src/main/resources/db/changelog/changeset/0009_topic_add_titles/topic_add_titles.sql
+++ src/main/resources/db/changelog/changeset/0009_topic_add_titles/topic_add_titles.sql
@@ -1,5 +1,5 @@
 ALTER TABLE consultingtypeservice.`topic`
-    ADD titles_short varchar (255) NOT NULL,
-    ADD titles_long varchar (255) NOT NULL,
-    ADD titles_welcome varchar (255) NOT NULL,
-    ADD titles_dropdown varchar (255) NOT NULL;
+    ADD titles_short varchar (255) NULL,
+    ADD titles_long varchar (255) NULL,
+    ADD titles_welcome varchar (255) NULL,
+    ADD titles_dropdown varchar (255)  NULL;
