ae179568c448aee01d91dcb4322948ab520d7ccb
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-07-05 14:25:06 +0200

    chore: change sql scripts to create titles

--- src/main/resources/db/changelog/changeset/0009_topic_add_titles/topic_add_titles-rollback.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE consultingtypeservice.`topic`
-    DROP IF EXISTS titles_short,
-    DROP IF EXISTS titles_long,
-    DROP IF EXISTS titles_welcome,
-    DROP IF EXISTS titles_dropdown;
---
+++ src/main/resources/db/changelog/changeset/0009_topic_add_titles_and_extend_internal_identifier/topic_add_titles_and_extend_internal_identifier-rollback.sql
@@ -0,0 +1,9 @@
+ALTER TABLE consultingtypeservice.`topic`
+    DROP IF EXISTS titles_short,
+    DROP IF EXISTS titles_long,
+    DROP IF EXISTS titles_welcome,
+    DROP IF EXISTS titles_dropdown
+    DROP IF EXISTS slug;
+
+ALTER TABLE consultingtypeservice.`topic`
+ALTER COLUMN internal_identifier varchar(50);
