51647293341409d231e49c4d8b83d5caaeb72d86
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-07-05 14:29:13 +0200

    chore: change sql scripts to create titles

--- src/main/resources/db/changelog/changeset/0009_topic_add_titles_and_extend_internal_identifier/topic_add_titles_and_extend_internal_identifier.sql
+++ src/main/resources/db/changelog/changeset/0009_topic_add_titles_and_extend_internal_identifier/topic_add_titles_and_extend_internal_identifier.sql
@@ -2,7 +2,7 @@ ALTER TABLE consultingtypeservice.`topic`
     ADD titles_short varchar (255) NULL,
     ADD titles_long varchar (255) NULL,
     ADD titles_welcome varchar (255) NULL,
-    ADD titles_dropdown varchar (255)  NULL
+    ADD titles_dropdown varchar (255)  NULL,
     ADD slug varchar(255) NULL;
 
 ALTER TABLE consultingtypeservice.`topic`
