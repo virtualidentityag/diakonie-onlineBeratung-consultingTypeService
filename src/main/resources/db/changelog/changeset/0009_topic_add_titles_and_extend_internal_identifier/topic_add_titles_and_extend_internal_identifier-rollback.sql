b2e3222f37a32671e90d1530af54df19570cd06e
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-07-05 14:26:14 +0200

    chore: change sql scripts to create titles

--- src/main/resources/db/changelog/changeset/0009_topic_add_titles_and_extend_internal_identifier/topic_add_titles_and_extend_internal_identifier-rollback.sql
+++ src/main/resources/db/changelog/changeset/0009_topic_add_titles_and_extend_internal_identifier/topic_add_titles_and_extend_internal_identifier-rollback.sql
@@ -2,7 +2,7 @@ ALTER TABLE consultingtypeservice.`topic`
     DROP IF EXISTS titles_short,
     DROP IF EXISTS titles_long,
     DROP IF EXISTS titles_welcome,
-    DROP IF EXISTS titles_dropdown
+    DROP IF EXISTS titles_dropdown,
     DROP IF EXISTS slug;
 
 ALTER TABLE consultingtypeservice.`topic`
